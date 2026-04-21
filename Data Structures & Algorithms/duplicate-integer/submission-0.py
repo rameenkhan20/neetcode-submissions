721b589a494c19ffee215ec4685a9090fdbce04e
Rameen Khan <113280583+rameenkhan20@users.noreply.github.com>
2026-04-21 20:23:32 +0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        freq = {}
+        for num in nums:
+            if num in freq:
+                return True
+            else:
+                freq[num] = 1
+        return False
+        
